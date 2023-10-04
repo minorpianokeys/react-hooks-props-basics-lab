a9883217c27000906f757beb38c48ba93807ca45
Michael Friedman <minorpianokeys@gmail.com>
2023-10-04 16:54:11 -0400

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+
+
+function About(props) {
+  function displayBio() {
+    if (!props.bio) {
+      return null
+    } else {
+      return <p>{props.bio}</p>
+    }
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {displayBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links 
+      github={props.github}
+      linkedin={props.linkedin}
+      />
     </div>
   );
 }
